c3c5830aba40b3f8004457ed5315d8864ff6b901
taehyun <kimtaehyun002@gmail.com>
2025-02-14 13:03:35 +0900

    [Feat] 관광 카테고리 선택 구현 및 장소 없음 예외처리 추가

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const app = express();
+import { TOUR_TYPE } from "../source/constant.js";
 
 // 미들웨어 설정
 app.use(express.json());
@@ -15,8 +16,9 @@ app.use(
   })
 );
 
-async function makeReply(text) {
+async function makeReply(text, type) {
   const url = `https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key=${process.env.GEMINI_API_KEY}`;
+  const typeText = TOUR_TYPE[type];
 
   const response = await axios({
     url,
@@ -26,21 +28,21 @@ async function makeReply(text) {
         {
           parts: [
             {
-              text: `숙소 정보와 반려동물 정보를 분석하여, 해당 동물이 방문 가능한 숙소 번호들을 공백으로 구분하여 반환해주세요.
-   
-   ### Instruction ###
-   당신은 RTF(Return To Filter) 엔진입니다. 제공된 정보를 바탕으로 방문 가능한 숙소들의 번호만을 반환해야 합니다.
-   
-   ### Rules ###
-   1. 숙소의 반려동물 정책과 주어진 반려동물 정보를 비교하세요
-   2. 모든 조건을 만족하는 숙소 번호만 선택하세요
-   3. 숙소 번호는 오름차순으로 정렬하세요
-   4. 숙소 번호만 공백으로 구분하여 반환하세요
-   
-   ### Output Format ###
-   RESULT:::[숙소번호1 숙소번호2 숙소번호3 ...]
-   
-   {${text}}`,
+              text: `${typeText} 정보와 반려동물 정보를 분석하여, 해당 동물이 방문 가능한 ${typeText} 번호들을 공백으로 구분하여 반환해주세요.
+  
+  ### Instruction ###
+  당신은 RTF(Return To Filter) 엔진입니다. 제공된 정보를 바탕으로 방문 가능한 ${typeText}들의 번호만을 반환해야 합니다.
+  
+  ### Rules ###
+  1. ${typeText}의 반려동물 정책과 주어진 반려동물 정보를 비교하세요
+  2. 모든 조건을 만족하는 ${typeText} 번호만 선택하세요
+  3. ${typeText} 번호는 오름차순으로 정렬하세요
+  4. ${typeText} 번호만 공백으로 구분하여 반환하세요
+  
+  ### Output Format ###
+  RESULT:::[${typeText}번호1 ${typeText}번호2 ${typeText}번호3 ...]
+  
+  {${text}}`,
             },
           ],
         },
@@ -61,10 +63,11 @@ app.post("/", (req, res) => {
   res.json({ body });
 });
 
-app.post("/gemini", async (req, res) => {
+app.post("/gemini/:type", async (req, res) => {
+  const type = req.params.type;
   const { text } = req.body;
   try {
-    const reply = await makeReply(text);
+    const reply = await makeReply(text, type);
     console.log(reply);
     const numbers = reply
       .split("RESULT:::")[1]
